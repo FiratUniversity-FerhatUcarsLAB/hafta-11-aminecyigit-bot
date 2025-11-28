c4b92b03c70267646b09c0273016fa2c79984f67
Amine Ceren Yiğit <aminecyigit@gmail.com>
2025-11-28 18:55:17 +0300

    Exercise4_4
    Added comments explaining behavior of methods in Java.

--- Exercise4_4.java
+++ Exercise4_4.java
@@ -12,9 +12,13 @@ public class Exercise4_4 {
 
         // 1) Donus degerini kullanmayin -> ne oluyor?
         getNumber();
+        // ** hiçbir şey olmaz. Metot tanımlı kalır. metodu çağırmak zorunda değiliz.
 
         // 2) void metodu ifade icinde kullanin -> ne oluyor?
         // System.out.println(sayHello() + 7);
+        //** void türüne izin verilmediğine dair hata verir.Bu hatanın temel sebebi void'in değer döndürmemesidir.
+        //void kullandığımız metodu tek başına çağırabiliriz. Değer döndürmez.
+        
 
         // Cevaplarinizi yorum olarak ekleyin.
     }
